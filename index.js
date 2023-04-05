1c2e12530ff6042a80f0a268fd9c3e0d25e04f79
Kevin <klums24@gmail.com>
2023-04-05 16:05:49 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+//define currentUser
+const currentUser = "Grace Hopper";
+
+//define welcomeMessage
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+//define excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//define shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
